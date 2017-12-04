094d4d7f677127de5c484dd65eda5119defe9387
Chris4ever <krzychnowakowski@gmail.com>
2017-12-04 10:44:45 +0100

    Dodanie Animation

--- SpaceWarrior/Animation.cpp
+++ SpaceWarrior/Animation.cpp
@@ -6,6 +6,32 @@ Animation::Animation()
 {
 }
 
+Animation(Texture &t, int x, int y, int w, int h, int count, float Speed)
+{
+	Frame = 0;
+	speed = Speed;
+
+	for (int i = 0; i<count; i++)
+		frames.push_back(IntRect(x + i*w, y, w, h));
+
+	sprite.setTexture(t);
+	sprite.setOrigin(w / 2, h / 2);
+	sprite.setTextureRect(frames[0]);
+}
+
+bool Animation::isEnd()
+{
+	return Frame + speed >= frames.size();
+}
+
+void Animation::update()
+{
+	Frame += speed;
+	int n = frames.size();
+	if (Frame >= n) Frame -= n;
+	if (n>0) sprite.setTextureRect(frames[int(Frame)]);
+}
+
 
 Animation::~Animation()
 {
--- SpaceWarrior/Animation.h
+++ SpaceWarrior/Animation.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <SFML\Graphics.hpp>
+
+using namespace sf;
+
 class Animation
 {
+private:
+	Sprite sprite;
+	float Frame, speed;
+	std::vector<IntRect> frames;
 public:
 	Animation();
 	~Animation();
+	Animation() {}
+	void update();
+	bool isEnd();
 };
 
